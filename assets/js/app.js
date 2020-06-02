7dad1a9cf5534168ed20df543c82225471cde3b6
Lili <lwall@nossi.edu>
2020-06-02 14:46:50 -0500

    added js functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,26 @@
 /* app.js */
 
 // create a variable motd and assign a string to it
-let motd = "Have a goodun, Joel!";
+let motdArray = [ 
+    "You miss 100% of the shots you don't take. <i>--Wayne Gretsky</i>",
+    "Everything you can imagine is real. <i>--Pablo Picasso</i>",
+    "Rudeness is merely the expression of fear. <i>--M. Gustave",
+    "Unless someonelike you cares a whole awful location, nothing is going to get ByteLengthQueuingStrategy, it's not. <i>--Dr. Seuss</i>",
+    "Find something you love to do and then do it for the rest of your life. <i> --Max Fisher",
+    "Tina, you fat lard, come get some dinner. <i>--Napoleon Dynamite</i>"
+];
+
+
+// get RandomInt function procured from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(max) {
+
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+let randomIndex = getRandomInt(motdArray.length);
+
+console.log("randomIndex is" + randomIndex);
 
 //query the document and get an element with an id of 'motd'
 // and assign our character string to it.
-document.querySelector("#motd").innerHTML = motd;
+document.querySelector("#motd").innerHTML = motdArray[randomIndex];
